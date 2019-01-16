4dbc22d8e77ddcf440a4fe4844c6de54a49c5b77
Dev Jhaveri <dev.jhaveri@gmail.com>
2019-01-16 13:26:26 -0500

    Started ChainBlock Project
    Added Basic Blockchain Functionality from tutorial:
    https://blockgeeks.com/guides/python-blockchain-2/

---
+++ Chainblock.py
@@ -0,0 +1,62 @@
+#   ChainBlock.py
+#   Defines the Basic Methods of the Blockchain according to the first step of the tutorial:
+#   https://blockgeeks.com/guides/python-blockchain-2/
+
+def add_list(blockchain):
+
+    blockchain.append([blockchain[-1], 3.2])
+    print(blockchain)
+
+def get_last_value(blockchain):
+
+   return(blockchain[-1])
+
+def add_value(blockchain, transaction_amount, last_transaction=[1]):
+
+   blockchain.append([last_transaction, transaction_amount])
+
+def get_transaction_value():
+
+   user_value = float(input('Enter your transaction amount '))
+   return user_value
+
+def get_user_choice():
+
+   user_input = input("Please give your choice here: ")
+   return user_input
+
+def print_block(blockchain):
+
+   for block in blockchain:
+
+       print("Here is your block")
+
+       print(block)
+
+       def verify_chain():
+
+           index = 0
+
+           valid = True
+
+           for block in blockchain:
+
+               if index == 0:
+
+                   index += 1
+
+                   continue
+
+               elif block[0] == blockchain[index - 1]:
+
+                   valid = True
+
+               else:
+
+                   valid = False
+
+                   break
+
+               index += 1
+
+           return valid
---
+++ main.py
@@ -0,0 +1,65 @@
+# Main.py
+# Initializes and creates the running loop of the blockchain.
+# Taken directly from https://blockgeeks.com/guides/python-blockchain-2/
+
+from Chainblock import *
+
+tx_amount = get_transaction_value()
+
+add_value(tx_amount)
+
+user_input = input("Give the first element of the Blockchain")
+
+blockchain = [user_input]
+
+while True:
+
+   print("Choose an option")
+
+   print('Choose 1 for adding a new transaction')
+
+   print('Choose 2 for printing the blockchain')
+
+   print('Choose 3 if you want to manipulate the data')
+
+   print('Choose anything else if you want to quit')
+
+
+
+   user_choice = get_user_choice()
+
+
+
+   if user_choice == 1:
+
+       tx_amount = get_transaction_value()
+
+       add_value(blockchain, tx_amount, get_last_value(blockchain))
+
+
+
+   elif user_choice == 2:
+
+       print_block(blockchain)
+
+
+
+   elif user_choice == 3:
+
+       if len(blockchain) >= 1:
+
+           blockchain[0] = 2
+
+
+
+   else:
+
+       break
+
+
+
+   if not verify_chain(blockchain):
+
+       print('Blockchain manipulated')
+
+       break
